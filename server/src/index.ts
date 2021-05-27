9da20247c5bb01bcfe4597de23a9727fa9d47381
Lemuel Canady <lem.canady@gmail.com>
2021-05-27 09:09:31 -0700

    Figured out my package.json files for the monorepo

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import { server, express, app } from "@ursamu/core";
+import path from "path";
 
-app.use(express.static("client"));
+app.use(express.static(path.resolve(__dirname, "../../client/build")));
 
 server.listen(4201);
